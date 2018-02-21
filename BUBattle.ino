1c5dc94d1bf6109001645ba71218158a997c616b
Jason Morris <jmmorris2@mail.bradley.edu>
2018-02-20 20:56:43 -0600

    Display home score and period

--- BUBattle.ino
+++ BUBattle.ino
@@ -10,13 +10,19 @@ Adafruit_7segment matrix = Adafruit_7segment();
 
 int awayTensBoard[7] = {2, 3, 4, 5, 6, 7, 8};
 int awayOnesBoard[7] = {9, 10, 11, 12, 13, 22, 23};
+int homeTensBoard[7] = {24, 25, 26, 27, 28, 29, 30};
+int homeOnesBoard[7] = {32, 33, 34, 35, 36, 37, 38};
+int periodBoard[7] = {40, 41, 42, 43, 44, 45, 46};
 
 int awayTens;
 int awayOnes;
+int homeTens;
+int homeOnes;
 int minutesTens;
 int minutesOnes;
 int secondsTens;
 int secondsOnes;
+int periodNum;
 
 String data;
 String homeScore;
@@ -30,6 +36,9 @@ void setup() {
   for (int i = 0; i < 7; i++) {
     pinMode(awayTensBoard[i], OUTPUT);
     pinMode(awayOnesBoard[i], OUTPUT);
+    pinMode(homeTensBoard[i], OUTPUT);
+    pinMode(homeOnesBoard[i], OUTPUT);
+    pinMode(periodBoard[i], OUTPUT);
   }
 
   Serial.begin(9600);
@@ -51,13 +60,19 @@ void loop() {
     if(data != "data") return;
     awayTens = awayScore[awayScore.length()-2] - '0';
     awayOnes = awayScore[awayScore.length()-1] - '0';
+    homeTens = homeScore[homeScore.length()-2] - '0';
+    homeOnes = homeScore[homeScore.length()-1] - '0';
     minutesTens = minutes[0] - '0';
     minutesOnes = minutes[1] - '0';
     secondsTens = seconds[0] - '0';
     secondsOnes = seconds[1] - '0';
+    periodNum = period.toInt();
     
     displayNumber[awayTens](awayTensBoard);
     displayNumber[awayOnes](awayOnesBoard);
+    displayNumber[homeTens](homeTensBoard);
+    displayNumber[homeOnes](homeOnesBoard);
+    displayNumber[periodNum](periodBoard);
 
     matrix.writeDigitNum(0, minutesTens, false);
     matrix.writeDigitNum(1, minutesOnes, false);
